90687b4c9ac42aa1ea2bc465ce870b9c1ced4616
sadiki-dev <sadiki.dev@gmail.com>
2021-04-30 18:43:42 +0000

    fix : update pointsfors selector

--- src/places_crawler.js
+++ src/places_crawler.js
@@ -293,16 +293,16 @@ const extractPlaceDetail = async ({ page, request }) => {
     let highlightSelector = await page.$eval("h2.section-subheader-header",el => el.innerText);
     if (highlightSelector == "Points forts" || highlightSelector == "Highlights" ){
     try {
-        await page.waitForSelector("div.uDxUUUCO4ji__container");
+        // await page.waitForSelector("div.uDxUUUCO4ji__container");
         await page.waitForNavigation();
         let pointsfortsList = await page.evaluate(() => {
-            let pointsFortsDOM = document.querySelector("div.uDxUUUCO4ji__container").innerText.trim().split("\n");
+            let pointsFortsDOM = [...document.querySelectorAll("div[class*='Hoteljustification__text']")].map(el => el.innerText);
             return pointsFortsDOM;
         });
         pointsforts=pointsfortsList;
 
     } catch (error) { 
-        pointsforts={};
+        pointsforts=[];
     }
     
     // let pointsfortsList = await page.$eval("div.uDxUUUCO4ji__container",el => el.innerText.trim().split("\n"));
